bfcb251e617aad67c510cc71f7c949f70817e06b
Pablo Padilla <Padilla.Pablo1998@gmail.com>
2021-12-24 19:43:38 -0800

    Add collisions

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
 
     public float moveSpeed;
+    public LayerMask solidObjectsLayer;
 
     private bool isMoving;
     private Vector2 input;
@@ -37,7 +38,10 @@ public class PlayerController : MonoBehaviour
                 targetPos.x += input.x;
                 targetPos.y += input.y;
 
+                if(IsWalkable(targetPos))
+                {
                 StartCoroutine(Move(targetPos));
+                }
             }
         }
 
@@ -56,5 +60,15 @@ public class PlayerController : MonoBehaviour
         transform.position = targetPos;
 
         isMoving = false;
+    }
+
+    private bool IsWalkable(Vector3 targetPos)
+    {
+        if (Physics2D.OverlapCircle(targetPos, 0.2f, solidObjectsLayer) != null)
+        {
+            return false;
+        }
+
+        return true;
     }
 }
